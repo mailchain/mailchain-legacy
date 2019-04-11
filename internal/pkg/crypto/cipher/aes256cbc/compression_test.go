695d8e4542cf520d0228d61c5f15be18b4d6f4d7
Rob De Feo <robdefeo@gmail.com>
2019-04-11 23:26:23 +0100

    skip errors on testing of decompress too

--- internal/pkg/crypto/cipher/aes256cbc/compression_test.go
+++ internal/pkg/crypto/cipher/aes256cbc/compression_test.go
@@ -42,24 +42,20 @@ func TestDecompress(t *testing.T) {
 		name     string
 		original []byte
 		expected []byte
-		err      error
 	}{
 		{"022c8432ca28ce929b86a47f2d40413d161f591f8985229060491573d83f82f292",
 			testutil.MustHexDecodeString("022c8432ca28ce929b86a47f2d40413d161f591f8985229060491573d83f82f292"),
 			testutil.MustHexDecodeString("042c8432ca28ce929b86a47f2d40413d161f591f8985229060491573d83f82f292f4dc68f918446332837aa57cd5145235cc40702d962cbb53ac27fb2246fb6cba"),
-			nil,
 		},
 		{"03a34d6aef3eb42335fb3cacb59478c0b44c0bbeb8bb4ca427dbc7044157a5d24b",
 			testutil.MustHexDecodeString("03a34d6aef3eb42335fb3cacb59478c0b44c0bbeb8bb4ca427dbc7044157a5d24b"),
 			testutil.MustHexDecodeString("04a34d6aef3eb42335fb3cacb59478c0b44c0bbeb8bb4ca427dbc7044157a5d24b4adf14868d8449c9b3e50d3d6338f3e5a2d3445abe679cddbe75cb893475806f"),
-			nil,
 		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			actual, err := decompress(tc.original)
+			actual := decompress(tc.original)
 			assert.EqualValues(hex.EncodeToString(tc.expected), hex.EncodeToString(actual))
-			assert.Equal(tc.err, err)
 		})
 	}
 }

c59497de228a525e2a943cb14e9136e0580a920e
Rob De Feo <robdefeo@gmail.com>
2019-04-11 21:52:03 +0100

    secp public from []byte

--- internal/pkg/crypto/keys/secp256k1/public.go
+++ internal/pkg/crypto/keys/secp256k1/public.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pkg/errors"
 )
 
 // PublicKey based on the secp256k1 curve
@@ -19,4 +20,13 @@ func (pk PublicKey) Bytes() []byte {
 // Address returns the byte representation of the address
 func (pk PublicKey) Address() []byte {
 	return crypto.PubkeyToAddress(pk.ecdsa).Bytes()
+}
+
+// PublicKeyFromBytes create a public key from []byte
+func PublicKeyFromBytes(pk []byte) (*PublicKey, error) {
+	rpk, err := crypto.UnmarshalPubkey(pk)
+	if err != nil {
+		return nil, errors.WithMessage(err, "could not convert pk")
+	}
+	return &PublicKey{ecdsa: *rpk}, nil
 }

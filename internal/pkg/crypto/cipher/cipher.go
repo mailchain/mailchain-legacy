a5ba40c811f139872f6d46362a2bab7dae1a7e78
Rob De Feo <robdefeo@gmail.com>
2019-04-13 16:49:53 +0100

    cipher mock

--- internal/pkg/crypto/cipher/cipher.go
+++ internal/pkg/crypto/cipher/cipher.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate mockgen -source=crypto.go -package=mocks -destination=$PACKAGE_PATH/internal/pkg/testutil/mocks/cipher.go
+//go:generate mockgen -source=cipher.go -package=mocks -destination=$PACKAGE_PATH/internal/pkg/testutil/mocks/cipher.go
 package cipher
 
 import (

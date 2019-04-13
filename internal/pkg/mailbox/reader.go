5054e829a56a7369d54113f58295da64a2773fd7
Rob De Feo <robdefeo@gmail.com>
2019-04-13 08:46:39 +0100

    get any message

--- internal/pkg/mailbox/reader.go
+++ internal/pkg/mailbox/reader.go
@@ -1,10 +1,30 @@
 package mailbox
 
 import (
+	"io/ioutil"
+	"net/url"
+
 	"github.com/pkg/errors"
 	"gopkg.in/resty.v1"
 )
 
+func getAnyMessage(location string) ([]byte, error) {
+	parsed, err := url.Parse(location)
+	if err != nil {
+		return nil, err
+	}
+
+	switch parsed.Scheme {
+	case "http":
+		return getHTTPMessage(location)
+	case "file":
+		return ioutil.ReadFile(parsed.Host + parsed.Path)
+	case "test":
+		return []byte(parsed.Host), nil
+	default:
+		return nil, errors.Errorf("unsupported scheme")
+	}
+}
 func getHTTPMessage(location string) ([]byte, error) {
 	res, err := resty.R().Get(location)
 	if err != nil {

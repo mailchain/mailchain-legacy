75b4b49c52381e1e43265c312b890553e887cc35
Rob De Feo <robdefeo@gmail.com>
2019-04-14 21:54:52 +0100

    senders config

---
+++ cmd/mailchain/config/clients.go
@@ -0,0 +1,31 @@
+// Copyright 2019 Finobo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/mailchain/mailchain/internal/pkg/clients/ethrpc"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper" // nolint: depguard
+)
+
+func etherRPC2Client(network string) (*ethrpc.EthRPC2, error) {
+	address := viper.GetString(fmt.Sprintf("clients.ethereum-rpc2.%s.address", network))
+	if address == "" {
+		return nil, errors.Errorf("`clients.ethereum-rpc2.%s.address` must not be empty", network)
+	}
+	return ethrpc.New(address)
+}
---
+++ cmd/mailchain/config/senders.go
@@ -0,0 +1,62 @@
+// Copyright 2019 Finobo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/imdario/mergo"
+	"github.com/mailchain/mailchain/cmd/mailchain/config/names"
+	"github.com/mailchain/mailchain/internal/pkg/mailbox"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper" // nolint: depguard
+)
+
+// Senders in configured state
+func Senders() (map[string]mailbox.Sender, error) {
+	senders := make(map[string]mailbox.Sender)
+	for chain := range viper.GetStringMap("chains") {
+		chSenders, err := chainSenders(chain)
+		if err != nil {
+			return nil, err
+		}
+		if err := mergo.Merge(&senders, chSenders); err != nil {
+			return nil, err
+		}
+	}
+	return senders, nil
+}
+
+func chainSenders(chain string) (map[string]mailbox.Sender, error) {
+	senders := make(map[string]mailbox.Sender)
+	for network := range viper.GetStringMap(fmt.Sprintf("chains.%s.networks", chain)) {
+		sender, err := sender(chain, network)
+		if err != nil {
+			return nil, err
+		}
+		senders[fmt.Sprintf("%s.%s", chain, network)] = sender
+	}
+
+	return senders, nil
+}
+
+func sender(chain, network string) (mailbox.Sender, error) {
+	switch viper.GetString(fmt.Sprintf("chains.%s.networks.%s.sender", chain, network)) {
+	case names.EthereumRPC2:
+		return etherRPC2Client(network)
+	default:
+		return nil, errors.Errorf("unsupported receiver")
+	}
+}

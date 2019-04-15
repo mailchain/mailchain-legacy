6f4489932e186c462fb9c9cb60b72ed863e016bc
Rob De Feo <robdefeo@gmail.com>
2019-04-15 22:33:50 +0100

    package doc

---
+++ internal/pkg/crypto/cipher/aes256cbc/aes256cbc.go
@@ -0,0 +1,16 @@
+// Copyright 2019 Finobo
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// package aes256cbc Advanced Encryption Standard, with a 256 bit key length, operating in the Chain Block Cipher mode (AES-256-CBC).
+package aes256cbc

84f4bcb5bfe8007891b4ac04c1ac4b3c1d3a2387
Rob De Feo <robdefeo@gmail.com>
2019-05-27 19:47:56 +0100

    address tests

--- cmd/mailchain/internal/http/handlers/read_test.go
+++ cmd/mailchain/internal/http/handlers/read_test.go
@@ -102,3 +102,46 @@ func Test_doRead(t *testing.T) {
 		})
 	}
 }
+
+// func TestPutRead(t *testing.T) {
+// 	assert := assert.New(t)
+// 	type args struct {
+// 		store stores.State
+// 	}
+// 	tests := []struct {
+// 		name             string
+// 		args             args
+// 		expectedResponse string
+// 		expectedStatus   int
+// 	}{
+// 		{
+// "success",
+// args{}
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			req, err := http.NewRequest("GET", "/", nil)
+// 			if err != nil {
+// 				t.Fatal(err)
+// 			}
+// 			// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+// 			rr := httptest.NewRecorder()
+// 			handler := http.HandlerFunc(PutRead(tt.args.store))
+
+// 			// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+// 			// directly and pass in our Request and ResponseRecorder.
+// 			handler.ServeHTTP(rr, req)
+
+// 			// Check the status code is what we expect.
+// 			if !assert.Equal(tt.expectedStatus, rr.Code) {
+// 				t.Errorf("handler returned wrong status code: got %v want %v",
+// 					rr.Code, tt.expectedStatus)
+// 			}
+// 			if !assert.Equal(tt.expectedResponse, rr.Body.String()) {
+// 				t.Errorf("handler returned unexpected body: got %v want %v",
+// 					rr.Body.String(), tt.expectedResponse)
+// 			}
+// 		})
+// 	}
+// }

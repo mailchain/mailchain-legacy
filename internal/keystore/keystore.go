7aaee0b4c951233b44d71a6387dcc631acf03689
Rob De Feo <robdefeo@gmail.com>
2019-05-27 12:46:59 +0100

    unit test keystore root package

--- internal/keystore/keystore.go
+++ internal/keystore/keystore.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+//go:generate mockgen -source=keystore.go -package=mocks -destination=$PACKAGE_PATH/internal/testutil/mocks/keystore.go
 package keystore
 
 import (

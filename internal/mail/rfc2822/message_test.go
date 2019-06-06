0aee5695710f4a13db9124a89a79f3d174b02f7e
Rob De Feo <robdefeo@gmail.com>
2019-06-06 22:16:04 +0100

    reply to test

--- internal/mail/rfc2822/message_test.go
+++ internal/mail/rfc2822/message_test.go
@@ -31,7 +31,9 @@ var update = flag.Bool("update", false, "update .golden files")
 
 func TestEncodeNewMessage(t *testing.T) {
 	assert := assert.New(t)
-	type args struct{ message *mail.Message }
+	type args struct {
+		message *mail.Message
+	}
 	tests := []struct {
 		name    string
 		args    args
@@ -49,6 +51,27 @@ func TestEncodeNewMessage(t *testing.T) {
 				Body: []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur maximus metus ante, sit amet ullamcorper dui hendrerit ac. Sed vestibulum dui lectus, quis eleifend urna mollis eu. Integer dictum metus ut sem rutrum aliquet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget euismod nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum venenatis sem sed auctor. Ut aliquam eu diam nec fermentum. Sed turpis nulla, viverra ac efficitur ac, fermentum vel sapien. Curabitur vehicula risus id odio congue tempor. Mauris tincidunt feugiat risus, eget auctor magna blandit sit amet. Curabitur consectetur, dolor eu imperdiet varius, dui neque mattis neque, vel fringilla magna tortor ut risus. Cras cursus sem et nisl interdum molestie. Aliquam auctor sodales blandit."),
 			}},
 			false,
+		},
+		{"reply-to",
+			args{&mail.Message{
+				Headers: &mail.Headers{
+					Date:    time.Date(2019, 3, 12, 20, 23, 13, 45, time.UTC),
+					From:    mail.Address{DisplayName: "", FullAddress: "5602ea95540bee46d03ba335eed6f49d117eab95c8ab8b71bae2cdd1e564a761@ropsten.ethereum", ChainAddress: ""},
+					To:      mail.Address{DisplayName: "", FullAddress: "4cb0a77b76667dac586c40cc9523ace73b5d772bd503c63ed0ca596eae1658b2@ropsten.ethereum", ChainAddress: ""},
+					ReplyTo: &mail.Address{DisplayName: "", FullAddress: "4cb0a77b76667dac586c40cc9523ace73b5d772bd503c63ed0ca596eae1658b2@mainnet.ethereum", ChainAddress: ""},
+					Subject: "Hello world",
+				},
+				ID:   mail.ID(testutil.MustHexDecodeMultiHashID("002c47eca011e32b52c71005ad8a8f75e1b44c92c99fd12e43bccfe571e3c2d13d2e9a826a550f5ff63b247af471")),
+				Body: []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur maximus metus ante, sit amet ullamcorper dui hendrerit ac. Sed vestibulum dui lectus, quis eleifend urna mollis eu. Integer dictum metus ut sem rutrum aliquet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus eget euismod nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum venenatis sem sed auctor. Ut aliquam eu diam nec fermentum. Sed turpis nulla, viverra ac efficitur ac, fermentum vel sapien. Curabitur vehicula risus id odio congue tempor. Mauris tincidunt feugiat risus, eget auctor magna blandit sit amet. Curabitur consectetur, dolor eu imperdiet varius, dui neque mattis neque, vel fringilla magna tortor ut risus. Cras cursus sem et nisl interdum molestie. Aliquam auctor sodales blandit."),
+			}},
+			false,
+		},
+		{
+			"err-nil-message",
+			args{
+				message: nil,
+			},
+			true,
 		},
 	}
 	for _, tt := range tests {

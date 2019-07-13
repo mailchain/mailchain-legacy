1689817401fbb357168393e8c546d3fe4e428565
Rob De Feo <robdefeo@gmail.com>
2019-07-13 13:27:23 +0100

    add unit tests for decode

--- internal/mail/rfc2822/message_test.go
+++ internal/mail/rfc2822/message_test.go
@@ -118,7 +118,16 @@ func TestDecodeNewMessage(t *testing.T) {
 			},
 			false,
 		},
-		// TODO: Add test cases.
+		{
+			"err-header",
+			nil,
+			true,
+		},
+		{
+			"err-id",
+			nil,
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {

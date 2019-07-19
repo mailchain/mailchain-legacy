24222283f97e88dd1141b0b52716aa9d5c5da5e4
Rob De Feo <robdefeo@gmail.com>
2019-07-19 20:10:00 +0100

    add kind definitions

--- internal/envelope/data.go
+++ internal/envelope/data.go
@@ -6,6 +6,11 @@ import (
 	"github.com/mailchain/mailchain/crypto/cipher"
 )
 
+const (
+	Kind0x01 byte = 0x01 // Message locator
+	Kind0x50 byte = 0x50 // Alpha
+)
+
 type Data interface {
 	URL(decrypter cipher.Decrypter) (*url.URL, error)
 	IntegrityHash(decrypter cipher.Decrypter) ([]byte, error)

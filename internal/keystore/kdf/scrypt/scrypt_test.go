ceb21785791c7c309ab653a2e5562bc9dc209a71
Rob De Feo <robdefeo@gmail.com>
2019-07-22 17:37:11 -0700

    key derive tests

--- internal/keystore/kdf/scrypt/scrypt_test.go
+++ internal/keystore/kdf/scrypt/scrypt_test.go
@@ -64,3 +64,64 @@ func TestApplyFromEncryptedKey(t *testing.T) {
 	assert.Equal(8, opts.R)
 	assert.Equal([]byte("salt-value"), opts.Salt)
 }
+
+func TestCreateOptions(t *testing.T) {
+	assert := assert.New(t)
+	type args struct {
+		o []DeriveOptionsBuilder
+	}
+	tests := []struct {
+		name string
+		args args
+		want *DeriveOpts
+	}{
+		{
+			"success",
+			args{
+				[]DeriveOptionsBuilder{WithPassphrase("passphrase"), DefaultDeriveOptions()},
+			},
+			&DeriveOpts{Len: 32, N: 262144, P: 1, R: 8, Salt: []uint8(nil), Passphrase: "passphrase"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CreateOptions(tt.args.o); !assert.Equal(tt.want, got) {
+				t.Errorf("CreateOptions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDeriveKey(t *testing.T) {
+	assert := assert.New(t)
+	type args struct {
+		o []DeriveOptionsBuilder
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{
+			"success",
+			args{
+				[]DeriveOptionsBuilder{WithPassphrase("passphrase"), DefaultDeriveOptions()},
+			},
+			[]byte{0x14, 0x64, 0xbb, 0x68, 0x5d, 0xd1, 0x6a, 0xef, 0xe9, 0x1a, 0xe1, 0x34, 0x6d, 0x3d, 0x9a, 0x60, 0x2, 0x43, 0x1a, 0x5a, 0x1b, 0xc2, 0xde, 0xa6, 0x23, 0x35, 0xef, 0xc8, 0xad, 0x1f, 0x60, 0x1b},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := DeriveKey(tt.args.o)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DeriveKey() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !assert.Equal(got, tt.want) {
+				t.Errorf("DeriveKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

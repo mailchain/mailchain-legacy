435a094394c8e4005dca6ccc069b95a894aac785
Rob De Feo <robdefeo@gmail.com>
2019-07-22 17:35:17 -0700

    posts tests

--- cmd/sentstore/handlers/post.go
+++ cmd/sentstore/handlers/post.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"bytes"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/mailchain/mailchain/cmd/sentstore/storage"
@@ -17,13 +20,14 @@ import (
 func PostHandler(base string, store storage.Store, maxContents int) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO: check message contents don't already exist
-		if len(r.URL.Query()["message-id"]) != 1 {
-			errs.JSONWriter(w, http.StatusPreconditionFailed, errors.Errorf("`message-id` must be specified once"))
+		messageID := []byte{}
+		if len(r.URL.Query()["contents-hash"]) != 1 {
+			errs.JSONWriter(w, http.StatusPreconditionFailed, errors.Errorf("`contents-hash` must be specified once"))
 			return
 		}
-		messageID, err := mail.FromHexString(r.URL.Query()["message-id"][0])
+		contentsHash, err := hex.DecodeString(r.URL.Query()["contents-hash"][0])
 		if err != nil {
-			errs.JSONWriter(w, http.StatusUnprocessableEntity, errors.WithMessage(err, "message-id invalid"))
+			errs.JSONWriter(w, http.StatusUnprocessableEntity, errors.WithMessage(err, "contents-hash invalid"))
 			return
 		}
 
@@ -36,48 +40,59 @@ func PostHandler(base string, store storage.Store, maxContents int) func(w http.
 			errs.JSONWriter(w, http.StatusPreconditionFailed, errors.Errorf("`hash` must not be empty"))
 			return
 		}
-
-		contents, err := ioutil.ReadAll(r.Body)
+		contents, status, err := getContents(r.Body, maxContents)
 		if err != nil {
-			errs.JSONWriter(w, http.StatusInternalServerError, errors.WithMessage(err, "contents can not be read"))
+			errs.JSONWriter(w, status, err)
 			return
 		}
-		if len(contents) == 0 {
-			errs.JSONWriter(w, http.StatusPreconditionFailed, errors.Errorf("`contents` must not be empty"))
-			return
-		}
-		if len(contents) > maxContents {
-			errs.JSONWriter(w, http.StatusUnprocessableEntity, errors.Errorf("file size too large"))
-			return
-		}
+
 		if err := compHash(contents, hash); err != nil {
 			errs.JSONWriter(w, http.StatusUnprocessableEntity, errors.WithMessage(err, "`hash` invalid"))
 			return
 		}
-		if err := store.Exists(messageID, contents, hash); err != nil {
+		if err := store.Exists(messageID, contentsHash, nil, contents); err != nil {
 			errs.JSONWriter(w, http.StatusConflict, errors.WithMessage(err, "conflict"))
 			return
 		}
 
-		loc, err := store.Put(messageID, contents, hash)
+		address, resource, mli, err := store.Put(messageID, contentsHash, nil, contents)
 		if err != nil {
 			errs.JSONWriter(w, http.StatusInternalServerError, errors.WithMessage(err, "failed to create message"))
 			return
 		}
-
-		w.Header().Set("Location", fmt.Sprintf("%s/%s", strings.TrimSuffix(base, "/"), loc))
+		if !strings.Contains(strings.TrimSuffix(address, "/"), resource) {
+			errs.JSONWriter(w, http.StatusConflict, errors.Errorf("location does not contain resource"))
+			return
+		}
+		w.Header().Set("Location", fmt.Sprintf("%s/%s", strings.TrimSuffix(base, "/"), resource))
+		w.Header().Set("Resource", resource)
+		w.Header().Set("Message-Location-Identifier", strconv.FormatUint(mli, 10))
 		w.WriteHeader(http.StatusCreated)
 	}
 }
 
+func getContents(body io.Reader, maxContents int) (contents []byte, status int, err error) {
+	contents, err = ioutil.ReadAll(body)
+	if err != nil {
+		return nil, http.StatusInternalServerError, errors.WithMessage(err, "contents can not be read")
+	}
+	if len(contents) == 0 {
+		return nil, http.StatusPreconditionFailed, errors.Errorf("`contents` must not be empty")
+	}
+	if len(contents) > maxContents {
+		return nil, http.StatusUnprocessableEntity, errors.Errorf("file size too large")
+	}
+	return contents, http.StatusOK, nil
+}
+
 func compHash(contents []byte, suppliedHex string) error {
-	contentsLocationHash := crypto.CreateLocationHash(contents)
+	integrityHash := crypto.CreateIntegrityHash(contents)
 	suppliedHash, err := mail.FromHexString(suppliedHex)
 	if err != nil {
 		return err
 	}
 
-	if !bytes.Equal(contentsLocationHash, suppliedHash) {
+	if !bytes.Equal(integrityHash, suppliedHash) {
 		return errors.Errorf("contents and supplied hash do not match")
 	}
 	return nil

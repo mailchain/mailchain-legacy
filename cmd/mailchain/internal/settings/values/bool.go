b02d7946f9295be8d3cfda504ed996f84b56143e
Rob De Feo <robdefeo@gmail.com>
2019-10-07 23:05:02 +0200

    values as attributes

--- cmd/mailchain/internal/settings/values/bool.go
+++ cmd/mailchain/internal/settings/values/bool.go
@@ -1,10 +1,17 @@
 // nolint:dupl
 package values
 
+import (
+	"strings"
+
+	"github.com/mailchain/mailchain/cmd/mailchain/internal/settings/output"
+)
+
 //go:generate mockgen -source=bool.go -package=valuestest -destination=./valuestest/bool_mock.go
 type Bool interface {
 	Get() bool
 	Set(v bool)
+	Attribute() output.Attribute
 }
 
 type DefaultBool struct {
@@ -22,6 +29,16 @@ func (d DefaultBool) Get() bool {
 
 func (d DefaultBool) Set(v bool) {
 	d.store.Set(d.setting, v)
+}
+
+func (d DefaultBool) Attribute() output.Attribute {
+	dots := strings.Split(d.setting, ".")
+
+	return output.Attribute{
+		Name:      dots[len(dots)-1],
+		IsDefault: d.Get() == d.def,
+		Value:     d.Get(),
+	}
 }
 
 func NewDefaultBool(defVal bool, store Store, setting string) Bool {
--- cmd/mailchain/internal/settings/values/int.go
+++ cmd/mailchain/internal/settings/values/int.go
@@ -1,10 +1,17 @@
 // nolint:dupl
 package values
 
+import (
+	"strings"
+
+	"github.com/mailchain/mailchain/cmd/mailchain/internal/settings/output"
+)
+
 //go:generate mockgen -source=int.go -package=valuestest -destination=./valuestest/int_mock.go
 type Int interface {
 	Get() int
 	Set(v int)
+	Attribute() output.Attribute
 }
 
 type DefaultInt struct {
@@ -22,6 +29,16 @@ func (d DefaultInt) Get() int {
 
 func (d DefaultInt) Set(v int) {
 	d.store.Set(d.setting, v)
+}
+
+func (d DefaultInt) Attribute() output.Attribute {
+	dots := strings.Split(d.setting, ".")
+
+	return output.Attribute{
+		Name:      dots[len(dots)-1],
+		IsDefault: d.Get() == d.def,
+		Value:     d.Get(),
+	}
 }
 
 func NewDefaultInt(defVal int, store Store, setting string) Int {

8159ba83c49b17370bf0a6e142ad3b3a9135503c
Rob De Feo <robdefeo@gmail.com>
2019-10-24 23:12:04 +0100

    fix build

--- cmd/mailchain/internal/settings/protocol.go
+++ cmd/mailchain/internal/settings/protocol.go
@@ -1,6 +1,8 @@
 package settings
 
 import (
+	"fmt"
+
 	"github.com/mailchain/mailchain/cmd/mailchain/internal/settings/output"
 	"github.com/mailchain/mailchain/cmd/mailchain/internal/settings/values"
 	"github.com/mailchain/mailchain/internal/mailbox"
@@ -8,7 +10,14 @@ import (
 	"github.com/mailchain/mailchain/sender"
 )
 
-func protocol(s values.Store, protocol string, networkClients map[string]NetworkClient) *Protocol
+func protocol(s values.Store, protocol string, networkClients map[string]NetworkClient) *Protocol {
+	return &Protocol{
+		Disabled: values.NewDefaultBool(false, s,
+			fmt.Sprintf("protocols.%s.disabled", protocol)),
+		Kind:     protocol,
+		Networks: networkClients,
+	}
+}
 
 type Protocol struct {
 	Networks map[string]NetworkClient
@@ -16,14 +25,81 @@ type Protocol struct {
 	Disabled values.Bool
 }
 
-func (p Protocol) GetSenders(senders *Senders) (map[string]sender.Message, error)
+func (p Protocol) GetSenders(senders *Senders) (map[string]sender.Message, error) {
+	msg := map[string]sender.Message{}
+	for network, v := range p.Networks {
+		s, err := v.ProduceSender(senders)
+		if err != nil {
+			return nil, err
+		}
+		msg[p.Kind+"/"+network] = s
+	}
+	return msg, nil
+}
 
-func (p Protocol) GetReceivers(receivers *Receivers) (map[string]mailbox.Receiver, error)
+func (p Protocol) GetReceivers(receivers *Receivers) (map[string]mailbox.Receiver, error) {
+	msg := map[string]mailbox.Receiver{}
+	for network, v := range p.Networks {
+		s, err := v.ProduceReceiver(receivers)
+		if err != nil {
+			return nil, err
+		}
+		msg[p.Kind+"/"+network] = s
+	}
+	return msg, nil
+}
 
-func (p Protocol) GetPublicKeyFinders(publicKeyFinders *PublicKeyFinders) (map[string]mailbox.PubKeyFinder, error)
+func (p Protocol) GetPublicKeyFinders(publicKeyFinders *PublicKeyFinders) (map[string]mailbox.PubKeyFinder, error) {
+	msg := map[string]mailbox.PubKeyFinder{}
+	for network, v := range p.Networks {
+		s, err := v.ProducePublicKeyFinders(publicKeyFinders)
+		if err != nil {
+			return nil, err
+		}
+		msg[p.Kind+"/"+network] = s
+	}
+	return msg, nil
+}
 
-func (p Protocol) GetAddressNameServices(ans *AddressNameServices) (map[string]nameservice.ReverseLookup, error)
+func (p Protocol) GetAddressNameServices(ans *AddressNameServices) (map[string]nameservice.ReverseLookup, error) {
+	msg := map[string]nameservice.ReverseLookup{}
+	for network, v := range p.Networks {
+		s, err := v.ProduceNameServiceAddress(ans)
+		if err != nil {
+			return nil, err
+		}
+		msg[p.Kind+"/"+network] = s
+	}
+	return msg, nil
+}
 
-func (p Protocol) GetDomainNameServices(ans *DomainNameServices) (map[string]nameservice.ForwardLookup, error)
+func (p Protocol) GetDomainNameServices(ans *DomainNameServices) (map[string]nameservice.ForwardLookup, error) {
+	msg := map[string]nameservice.ForwardLookup{}
+	for network, v := range p.Networks {
+		s, err := v.ProduceNameServiceDomain(ans)
+		if err != nil {
+			return nil, err
+		}
+		msg[p.Kind+"/"+network] = s
+	}
+	return msg, nil
+}
 
-func (p Protocol) Output() output.Element
+func (p Protocol) Output() output.Element {
+	networkElements := []output.Element{}
+	for _, c := range p.Networks {
+		networkElements = append(networkElements, c.Output())
+	}
+	return output.Element{
+		FullName: "protocols." + p.Kind,
+		Attributes: []output.Attribute{
+			p.Disabled.Attribute(),
+		},
+		Elements: []output.Element{
+			output.Element{
+				FullName: "protocols." + p.Kind + ".networks",
+				Elements: networkElements,
+			},
+		},
+	}
+}

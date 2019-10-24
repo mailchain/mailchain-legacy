810c4535ce12446590e3d0e824b5b507da66eaf5
Rob De Feo <robdefeo@gmail.com>
2019-10-24 19:56:53 +0100

    return just the pk bytes

--- crypto/ed25519/ed25519test/keys.go
+++ crypto/ed25519/ed25519test/keys.go
@@ -16,14 +16,14 @@ var CharlottePublicKey crypto.PublicKey   // nolint: gochecknoglobals
 // nolint: gochecknoinits
 func init() {
 	var err error
-	SofiaPrivateKey, err = ed25519.PrivateKeyFromSeed(testutil.MustHexDecodeString("0d9b4a3c10721991c6b806f0f343535dc2b46c74bece50a0a0d6b9f0070d3157"))
+	SofiaPrivateKey, err = ed25519.PrivateKeyFromBytes(testutil.MustHexDecodeString("0d9b4a3c10721991c6b806f0f343535dc2b46c74bece50a0a0d6b9f0070d3157"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	SofiaPublicKey = SofiaPrivateKey.PublicKey()
 
-	CharlottePrivateKey, err = ed25519.PrivateKeyFromSeed(testutil.MustHexDecodeString("39d4c97d6a7f9e3306a2b5aae604ee67ec8b1387fffb39128fc055656cff05bb"))
+	CharlottePrivateKey, err = ed25519.PrivateKeyFromBytes(testutil.MustHexDecodeString("39d4c97d6a7f9e3306a2b5aae604ee67ec8b1387fffb39128fc055656cff05bb"))
 	if err != nil {
 		log.Fatal(err)
 	}

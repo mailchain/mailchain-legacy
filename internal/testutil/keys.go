bc83faddf7e2f7e57c888f09d1eac25dfdb09a37
Developer Fred | @codingsh <developerfred@protonmail.ch>
2019-11-04 23:59:50 -0300

    fixed #337

--- internal/testutil/keys.go
+++ internal/testutil/keys.go
@@ -15,9 +15,9 @@
 package testutil
 
 import (
+	"encoding/hex"
 	"log"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/mailchain/mailchain/crypto"
 	"github.com/mailchain/mailchain/crypto/secp256k1"
 )
@@ -26,21 +26,23 @@ var SofiaPrivateKey crypto.PrivateKey     //nolint: gochecknoglobals
 var SofiaPublicKey crypto.PublicKey       //nolint: gochecknoglobals
 var CharlottePrivateKey crypto.PrivateKey //nolint: gochecknoglobals
 var CharlottePublicKey crypto.PublicKey   //nolint: gochecknoglobals
+var ecryptSofiaPrivateKey byte            //nolint: gochecknoglobals
+var encryptCharlottePrivateKey byte       //nolint: gochecknoglobals
 
 //nolint: gochecknoinits
 func init() {
 	var err error
-	ecryptSofiaPrivateKey, err := hexutil.Decode("01901E63389EF02EAA7C5782E08B40D98FAEF835F28BD144EECF5614A415943F")
+	ecryptSofiaPrivateKey, err := hex.DecodeString("01901E63389EF02EAA7C5782E08B40D98FAEF835F28BD144EECF5614A415943F")
 	if err != nil {
 		log.Fatal(err)
 	}
-	SofiaPrivateKey, err = secp256k1.PrivateKeyFromBytes(encryptSofiaPrivateKey)
+	SofiaPrivateKey, err = secp256k1.PrivateKeyFromBytes(ecryptSofiaPrivateKey)
 	if err != nil {
 		log.Fatal(err)
 	}
 	SofiaPublicKey = SofiaPrivateKey.PublicKey()
 
-	encryptCharlottePrivateKey, err := hexutil.Decode("DF4BA9F6106AD2846472F759476535E55C5805D8337DF5A11C3B139F438B98B3")
+	encryptCharlottePrivateKey, err := hex.DecodeString("DF4BA9F6106AD2846472F759476535E55C5805D8337DF5A11C3B139F438B98B3")
 	if err != nil {
 		log.Fatal(err)
 	}

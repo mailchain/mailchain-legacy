f031804e55adbaea4ec0ef5391825f69494b171c
lpfloyd <jeevan.pillay8@gmail.com>
2019-11-07 00:14:50 +1100

    Fixed datatype of encryption

--- cmd/mailchain/internal/http/handlers/send_message.go
+++ cmd/mailchain/internal/http/handlers/send_message.go
@@ -97,6 +97,7 @@ func SendMessage(sent stores.Sent, senders map[string]sender.Message, ks keystor
 		if err != nil {
 			errs.JSONWriter(w, http.StatusUnprocessableEntity, errors.WithStack(errors.WithMessage(err, "could not get `encrypter`")))
 		}
+
 		if err := mailbox.SendMessage(ctx, req.Protocol, req.Network,
 			msg, req.Body.publicKey,
 			encrypter, messageSender, sent, signer, envelope.Kind0x01); err != nil {
@@ -202,12 +203,14 @@ type PostMessage struct {
 // swagger:model SendMessageRequestBody
 type PostRequestBody struct {
 	// required: true
-	Message    PostMessage `json:"message"`
-	to         *mail.Address
-	from       *mail.Address
-	replyTo    *mail.Address
-	publicKey  crypto.PublicKey
-	encryption byte
+	Message   PostMessage `json:"message"`
+	to        *mail.Address
+	from      *mail.Address
+	replyTo   *mail.Address
+	publicKey crypto.PublicKey
+	// encryption method to use
+	// required: false
+	encryption byte `json:"encryption-method"`
 }
 
 func checkForEmpties(msg PostMessage) error {

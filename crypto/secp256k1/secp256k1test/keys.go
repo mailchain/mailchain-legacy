4eaae65d5ccccb433fc3356914a3feb84a6637cf
Fred @codingsh <57037080+developerfred@users.noreply.github.com>
2019-11-07 19:38:57 -0300

    #337 change call method PrivateKeyFromHex  to PrivateKeyFromBytes (#343)
    * #337 change call method PrivateKeyFromHex  to PrivateKeyFromBytes

--- crypto/secp256k1/secp256k1test/keys.go
+++ crypto/secp256k1/secp256k1test/keys.go
@@ -1,6 +1,7 @@
 package secp256k1test
 
 import (
+	"encoding/hex"
 	"log"
 
 	"github.com/mailchain/mailchain/crypto"
@@ -19,13 +20,26 @@ var CharlottePublicKey crypto.PublicKey //nolint: gochecknoglobals
 //nolint: gochecknoinits
 func init() {
 	var err error
-	SofiaPrivateKey, err = secp256k1.PrivateKeyFromHex("01901E63389EF02EAA7C5782E08B40D98FAEF835F28BD144EECF5614A415943F")
+	encryptedSofiaPrivateKey, err := hex.DecodeString("01901E63389EF02EAA7C5782E08B40D98FAEF835F28BD144EECF5614A415943F")
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	SofiaPrivateKey, err = secp256k1.PrivateKeyFromBytes(encryptedSofiaPrivateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	SofiaPublicKey = SofiaPrivateKey.PublicKey()
 
-	CharlottePrivateKey, err = secp256k1.PrivateKeyFromHex("DF4BA9F6106AD2846472F759476535E55C5805D8337DF5A11C3B139F438B98B3")
+	encryptedCharlottePrivateKey, err := hex.DecodeString("DF4BA9F6106AD2846472F759476535E55C5805D8337DF5A11C3B139F438B98B3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	CharlottePrivateKey, err = secp256k1.PrivateKeyFromBytes(encryptedCharlottePrivateKey)
+
 	if err != nil {
 		log.Fatal(err)
 	}

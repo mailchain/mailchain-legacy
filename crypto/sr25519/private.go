ceda80c00f16a7666a7076a0bb8551c3e703b456
Developer Fred <developerfred@protonmail.ch>
2019-11-10 15:07:50 -0300

    add files on go.mod and fixed ristreto to ristretto

--- crypto/sr25519/private.go
+++ crypto/sr25519/private.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mailchain/mailchain/crypto"
 
 	"github.com/gtank/merlin"
-	r255 "github.com/gtank/ristreto255"
+	r255 "github.com/gtank/ristretto255"
 )
 
 type MiniSecretKey struct {

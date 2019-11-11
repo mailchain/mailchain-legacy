46791dd82130d1177ca3a8d8cb54c2c82c14628f
Developer Fred <developerfred@protonmail.ch>
2019-11-11 12:21:26 -0300

    [WIP]test keys file

--- crypto/sr25519/sr25519test/keys.go
+++ crypto/sr25519/sr25519test/keys.go
@@ -9,22 +9,23 @@ import (
 )
 
 // SofiaPrivateKey ed25519 key for testing purposes. Key is compromised do not use on mainnet's.
-var SofiaPrivateKey crypto.PrivateKey //nolint: gochecknoglobals
+var SofiaPrivateKey sr25519.PrivateKey //nolint: gochecknoglobals
 // SofiaPublicKey ed25519 key for testing purposes. Key is compromised do not use on mainnet's.
-var SofiaPublicKey crypto.PublicKey //nolint: gochecknoglobals
+var SofiaPublicKey sr25519.PublicKey //nolint: gochecknoglobals
 // CharlottePrivateKey ed25519 key for testing purposes. Key is compromised do not use on mainnet's.
-var CharlottePrivateKey crypto.PrivateKey //nolint: gochecknoglobals
+var CharlottePrivateKey sr25519.PrivateKey //nolint: gochecknoglobals
 // CharlottePublicKey ed25519 key for testing purposes. Key is compromised do not use on mainnet's.
-var CharlottePublicKey crypto.PublicKey //nolint: gochecknoglobals
+var CharlottePublicKey sr25519.PublicKey //nolint: gochecknoglobals
 
 func int() {
+	var err error
 	msc, err := NewMiniSecretKeyFromRaw([32]byte{})
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	sc := msc.ExpandEd25519()
-	expected, err := sr25519.NewMiniSecretKey(testutil.MustHexDecodeString("0d9b4a3c10721991c6b806f0f343535dc2b46c74bece50a0a0d6b9f0070d3157")
+	expected, err := sr25519.NewMiniSecretKey(testutil.MustHexDecodeString("0d9b4a3c10721991c6b806f0f343535dc2b46c74bece50a0a0d6b9f0070d3157"))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -42,7 +43,19 @@ func int() {
 		t.Errorf("Fail to expand nonce: got %x expected %x", sc.nonce, expected[32:64])
 	}
 
+	SofiaPrivateKey, err = sr25519.PrivateKeyFromBytes(testutil.MustHexDecodeString("0d9b4a3c10721991c6b806f0f343535dc2b46c74bece50a0a0d6b9f0070d3157"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	SofiaPublicKey = SofiaPrivateKey.PublicKey()
+
+	CharlottePrivateKey, err = sr25519.PrivateKeyFromBytes(testutil.MustHexDecodeString("39d4c97d6a7f9e3306a2b5aae604ee67ec8b1387fffb39128fc055656cff05bb"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	CharlottePublicKey = CharlottePrivateKey.PublicKey()
 
 	
 }

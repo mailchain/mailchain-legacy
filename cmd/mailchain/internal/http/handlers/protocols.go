020e92458546a24fb8e91a56eac6ca559cc04524
Pascal <pascal.ondieki21@gmail.com>
2019-11-18 22:58:43 +0300

    resolve expressions being cuddled with blocks.

--- cmd/mailchain/internal/http/handlers/protocols.go
+++ cmd/mailchain/internal/http/handlers/protocols.go
@@ -51,6 +51,7 @@ func GetProtocols(base *settings.Root) func(w http.ResponseWriter, r *http.Reque
 		sort.Slice(networks, func(i, j int) bool {
 			return networks[i].Name < networks[j].Name
 		})
+		
 		resP := GetProtocolsProtocol{
 			Name:     protocol.Kind,
 			Networks: networks,

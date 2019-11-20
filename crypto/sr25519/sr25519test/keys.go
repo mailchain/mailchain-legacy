e74f1b2a3beed94c6754a50314997aa517691027
Developer Fred <developerfred@protonmail.ch>
2019-11-20 13:05:07 -0300

    add keys of thes sr25519

--- crypto/sr25519/sr25519test/keys.go
+++ crypto/sr25519/sr25519test/keys.go
@@ -6,7 +6,6 @@ import (
 	"github.com/mailchain/mailchain/crypto"
 	"github.com/mailchain/mailchain/crypto/sr25519"
 	"github.com/mailchain/mailchain/internal/testutil"
-	"github.com/mailchain/mailchain/internal/encoding"
 )
 
 // SofiaPrivateKey sr25519 key for testing purposes. Key is compromised do not use on mainnet's.
@@ -20,14 +19,16 @@ var CharlottePublicKey crypto.PublicKey //nolint: gochecknoglobals
 
 func int() {
 	var err error
-	SofiaPrivateKey, err = sr25519.PrivateKeyFromBytes(testutil.MustDecodeBase58("0x5c6d7adf75bda1180c225d25f3aa8dc174bbfb3cddee11ae9a85982f6faf791a"))
+	SofiaPrivateKey, err = sr25519.PrivateKeyFromBytes(testutil.MustHexDecodeStringTurbo("0x5c6d7adf75bda1180c225d25f3aa8dc174bbfb3cddee11ae9a85982f6faf791a"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Printf("%v'\n", SofiaPrivateKey)
+
 	SofiaPrivateKey = SofiaPrivateKey.PublicKey()
 
-	CharlottePrivateKey, err = sr25519.PrivateKeyFromBytes(testutil.MustDecodeBase58("0x23b063a581fd8e5e847c4e2b9c494247298791530f5293be369e8bf23a45d2bd"))
+	CharlottePrivateKey, err = sr25519.PrivateKeyFromBytes(testutil.MustHexDecodeStringTurbo("0x23b063a581fd8e5e847c4e2b9c494247298791530f5293be369e8bf23a45d2bd"))
 	if err != nil {
 		log.Fatal(err)
 	}

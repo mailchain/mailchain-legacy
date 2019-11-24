53d970338e42ca8a6fa4f271dbb85c9e2bea086c
Pascal <pascal.ondieki21@gmail.com>
2019-11-24 11:37:33 +0300

    changed switch to if statement

--- cmd/mailchain/internal/http/handlers/protocols.go
+++ cmd/mailchain/internal/http/handlers/protocols.go
@@ -38,13 +38,8 @@ func GetProtocols(base *settings.Root) func(w http.ResponseWriter, r *http.Reque
 			if !network.Disabled() && protocol.Kind == "ethereum" {
 				networks = append(networks, Network{Name: network.Kind(), ID: ""})
 			} else if !network.Disabled() && protocol.Kind == "substrate" {
-				switch network.Kind() {
-				case "edgeware-testnet":
+				if network.Kind() == "edgeware-testnet" {
 					networks = append(networks, Network{Name: network.Kind(), ID: "42"})
-				// case "polkadot-testnet":
-				// 	networks = append(networks, Network{Name: network.Kind(), ID: "0"})
-				// case "kusama-testnet":
-				// 	networks = append(networks, Network{Name: network.Kind(), ID: "2"})
 				}
 			}
 		}

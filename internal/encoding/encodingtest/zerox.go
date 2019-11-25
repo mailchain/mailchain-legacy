c1ed3740b5e3900824033d14797a88b34a61e34c
kifen <kifenraph@gmail.com>
2019-11-25 10:28:49 +0100

    refactor MustDecodeZeroX

--- internal/encoding/encodingtest/zerox.go
+++ internal/encoding/encodingtest/zerox.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testutil
+package encodingtest
 
 import "github.com/mailchain/mailchain/internal/encoding"
 

67e0d511ee1e6b8780016d7e3c1733f021034c5a
Developer Fred <developerfred@protonmail.ch>
2019-11-28 00:08:00 -0300

    sr25519test/keys.go

--- crypto/cipher/nacl/end_end_test.go
+++ crypto/cipher/nacl/end_end_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/mailchain/mailchain/crypto"
 	"github.com/mailchain/mailchain/crypto/ed25519/ed25519test"
-	"github.com/mailchain/mailchain/crypto/sr25519/sr25519test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -43,59 +42,6 @@ func TestEncryptDecrypt(t *testing.T) {
 			"to-charlotte-medium-text",
 			ed25519test.CharlottePublicKey,
 			ed25519test.CharlottePrivateKey,
-			[]byte("Hi Charlotte, this is a little bit of a longer message to make sure there are no problems"),
-			nil,
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			encrypted, err := NewEncrypter().Encrypt(tc.recipientPublicKey, tc.data)
-			assert.Equal(tc.err, err)
-			assert.NotNil(encrypted)
-			decrypter := Decrypter{tc.recipientPrivateKey}
-
-			decrypted, err := decrypter.Decrypt(encrypted)
-			assert.Equal(tc.err, err)
-			assert.Equal(tc.data, []byte(decrypted))
-		})
-	}
-}
-
-func TestEncryptDecryptSR25519(t *testing.T) {
-	assert := assert.New(t)
-	cases := []struct {
-		name                string
-		recipientPublicKey  crypto.PublicKey
-		recipientPrivateKey crypto.PrivateKey
-		data                []byte
-		err                 error
-	}{
-		{
-			"to-sofia-short-text",
-			sr25519test.SofiaPublicKey,
-			sr25519test.SofiaPrivateKey,
-			[]byte("Hi Sofia"),
-			nil,
-		},
-		{
-			"to-sofia-medium-text",
-			sr25519test.SofiaPublicKey,
-			sr25519test.SofiaPrivateKey,
-			[]byte("Hi Sofia, this is a little bit of a longer message to make sure there are no problems"),
-			nil,
-		},
-		{
-			"to-charlotte-short-text",
-			sr25519test.CharlottePublicKey,
-			sr25519test.CharlottePrivateKey,
-			[]byte("Hi Charlotte"),
-			nil,
-		},
-		{
-			"to-charlotte-medium-text",
-			sr25519test.CharlottePublicKey,
-			sr25519test.CharlottePrivateKey,
 			[]byte("Hi Charlotte, this is a little bit of a longer message to make sure there are no problems"),
 			nil,
 		},

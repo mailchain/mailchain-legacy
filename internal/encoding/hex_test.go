f99788d27b3157b7bcf2ab2f6784db3adf752d53
Fred @codingsh <developerfred@protonmail.ch>
2019-11-28 21:18:36 +0000

    change bytes

--- internal/encoding/hex_test.go
+++ internal/encoding/hex_test.go
@@ -106,15 +106,13 @@ func Test_EncodeHex(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		wantEncoded  []byte
 		wantEncoding string
 	}{
 		{
 			"success",
 			args{
-				[]byte{86, 2, 234, 149, 84, 11, 238, 70, 208, 59, 163, 53, 238, 214, 244, 157, 17, 126, 171, 149, 200, 171, 139, 113, 186, 226, 205, 209, 229, 100, 167, 97},
+				[]byte{0x56, 0x2, 0xea, 0x95, 0x54, 0xb, 0xee, 0x46, 0xd0, 0x3b, 0xa3, 0x35, 0xee, 0xd6, 0xf4, 0x9d, 0x11, 0x7e, 0xab, 0x95, 0xc8, 0xab, 0x8b, 0x71, 0xba, 0xe2, 0xcd, 0xd1, 0xe5, 0x64, 0xa7, 0x61},
 			},
-			[]byte{86, 2, 234, 149, 84, 11, 238, 70, 208, 59, 163, 53, 238, 214, 244, 157, 17, 126, 171, 149, 200, 171, 139, 113, 186, 226, 205, 209, 229, 100, 167, 97},
 			"5602ea95540bee46d03ba335eed6f49d117eab95c8ab8b71bae2cdd1e564a761",
 		},
 	}

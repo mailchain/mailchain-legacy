9911605f359180d8e3e60f08bf069bebb15d8d4a
Developer Fred <developerfred@protonmail.ch>
2019-11-29 11:10:10 -0300

    add encodingtest.MustDecodeHex on ed25519

--- crypto/ed25519/ed25519test/keys.go
+++ crypto/ed25519/ed25519test/keys.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/mailchain/mailchain/crypto"
 	"github.com/mailchain/mailchain/crypto/ed25519"
-	"github.com/mailchain/mailchain/internal/testutil"
+	"github.com/mailchain/mailchain/internal/encoding/encodingtest"
 )
 
 // SofiaPrivateKey ed25519 key for testing purposes. Key is compromised do not use on mainnet's.
@@ -20,14 +20,14 @@ var CharlottePublicKey crypto.PublicKey //nolint: gochecknoglobals
 //nolint: gochecknoinits
 func init() {
 	var err error
-	SofiaPrivateKey, err = ed25519.PrivateKeyFromBytes(testutil.MustHexDecodeString("0d9b4a3c10721991c6b806f0f343535dc2b46c74bece50a0a0d6b9f0070d3157"))
+	SofiaPrivateKey, err = ed25519.PrivateKeyFromBytes(encodingtest.MustDecodeHex("0d9b4a3c10721991c6b806f0f343535dc2b46c74bece50a0a0d6b9f0070d3157"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	SofiaPublicKey = SofiaPrivateKey.PublicKey()
 
-	CharlottePrivateKey, err = ed25519.PrivateKeyFromBytes(testutil.MustHexDecodeString("39d4c97d6a7f9e3306a2b5aae604ee67ec8b1387fffb39128fc055656cff05bb"))
+	CharlottePrivateKey, err = ed25519.PrivateKeyFromBytes(encodingtest.MustDecodeHex("39d4c97d6a7f9e3306a2b5aae604ee67ec8b1387fffb39128fc055656cff05bb"))
 	if err != nil {
 		log.Fatal(err)
 	}

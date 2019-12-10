ba8cd436c272bf2bc2d9538f1c6fe47f594ac0b1
Developer Fred <developerfred@protonmail.ch>
2019-12-10 19:55:24 -0300

    remove encoding and decoding

--- crypto/sr25519/public.go
+++ crypto/sr25519/public.go
@@ -11,6 +11,7 @@ const (
 	publicKeySize   = 32
 	signatureLength = 64
 	signatureSize   = 64
+	publicKeyLength = 32
 )
 
 // PublicKey is a interface
@@ -58,43 +59,13 @@ func (pk PublicKey) Verify(message, sig []byte) bool {
 	return pk.key.Verify(s, t)
 }
 
-// Encode returns the 32-byte encoding of the public key
-func (pk PublicKey) Encode() []byte {
-	enc := pk.key.Encode()
-
-	return enc[:]
-}
-
-// Decode decodes the input bytes into a public key and sets the receiver the decoded key
-// Input must be 32 bytes, or else this function will error
-func (pk PublicKey) Decode(in []byte) error {
-	b := [publicKeySize]byte{}
-	copy(b[:], in)
-
-	return pk.key.Decode(b)
-}
-
-func schnorrkelPublicKeyFromBytes(in []byte) (*schnorrkel.PublicKey, error) {
-	if len(in) != publicKeySize {
-		return nil, errors.New("input to sr25519 public key decode is not 32 bytes")
-	}
-
-	b := [32]byte{}
-	copy(b[:], in)
-
-	key := &schnorrkel.PublicKey{}
-	err := key.Decode(b)
-
-	return key, err
-}
-
 // PublicKeyFromBytes - Convert byte array to PublicKey
 func PublicKeyFromBytes(keyBytes []byte) (*PublicKey, error) {
 	switch len(keyBytes) {
 	case publicKeySize:
-		pubKey, _ := schnorrkelPublicKeyFromBytes(keyBytes)
+		pubKey := newPublicKey(keyBytes)
 
-		return &PublicKey{pubKey}, nil
+		return &(pubKey), nil
 	default:
 		return nil, errors.New("public key must be 32 bytes")
 	}

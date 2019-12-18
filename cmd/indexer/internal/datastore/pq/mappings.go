2df750f571065fa4358df67b75f9c160697d34ca
Rob De Feo <robdefeo@gmail.com>
2019-12-18 21:44:35 +0000

    put unit test

--- cmd/indexer/internal/datastore/pq/mappings.go
+++ cmd/indexer/internal/datastore/pq/mappings.go
@@ -3,7 +3,22 @@ package pq
 import (
 	"github.com/mailchain/mailchain/internal/protocols"
 	"github.com/mailchain/mailchain/internal/protocols/ethereum"
+	"github.com/pkg/errors"
 )
+
+func getProtocolNetworkUint8(prot, net string) (protocol, network uint8, err error) {
+	uProtocol, ok := protocolUint8[prot]
+	if !ok {
+		return 0, 0, errors.Errorf("unknown protocol: %q", prot)
+	}
+
+	uNetwork, ok := protocolNetworkUint8[prot][net]
+	if !ok {
+		return 0, 0, errors.Errorf("unknown protocol.network: \"%s.%s\"", prot, net)
+	}
+
+	return uProtocol, uNetwork, nil
+}
 
 var protocolUint8 = map[string]uint8{ //nolint:gochecknoglobals
 	protocols.Ethereum: 1,
---
+++ cmd/indexer/internal/datastore/pq/sync.go
@@ -0,0 +1,80 @@
+package pq
+
+import (
+	"context"
+	"time"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/jmoiron/sqlx"
+	"github.com/mailchain/mailchain/cmd/indexer/internal/datastore"
+	"github.com/pkg/errors"
+)
+
+// SyncStore database connection object
+type SyncStore struct {
+	db *sqlx.DB
+}
+
+// NewSyncStore create new postgres database
+func NewSyncStore(db *sqlx.DB) (datastore.SyncStore, error) {
+	return &SyncStore{db: db}, nil
+}
+
+type sync struct {
+	Protocol uint8 `db:"protocol"`
+	Network  uint8 `db:"network"`
+
+	BlockNo uint64 `db:"block_no"`
+
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+func (s SyncStore) GetBlockNumber(ctx context.Context, protocol, network string) (blockNo uint64, err error) {
+	p, n, err := getProtocolNetworkUint8(protocol, network)
+	if err != nil {
+		return 0, errors.WithStack((err))
+	}
+
+	sql, args, err := squirrel.Select("block_no").
+		From("sync").
+		PlaceholderFormat(squirrel.Dollar).
+		Where(squirrel.Eq{"protocol": p}).
+		Where(squirrel.Eq{"network": n}).
+		ToSql()
+	if err != nil {
+		return 0, errors.WithStack((err))
+	}
+
+	state := sync{}
+	if err := s.db.Get(&state, sql, args); err != nil {
+		return 0, errors.WithStack(err)
+	}
+
+	return state.BlockNo, nil
+}
+
+func (s SyncStore) PutBlockNumber(ctx context.Context, protocol, network string, blockNo uint64) error {
+	p, n, err := getProtocolNetworkUint8(protocol, network)
+	if err != nil {
+		return errors.WithStack((err))
+	}
+
+	sql, args, err := squirrel.Update("sync").
+		Set("block_no", blockNo).
+		Set("updated_at", time.Now()).
+		PlaceholderFormat(squirrel.Dollar).
+		Where(squirrel.Eq{"protocol": p}).
+		Where(squirrel.Eq{"network": n}).
+		ToSql()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	_, err = s.db.Exec(sql, args...)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}

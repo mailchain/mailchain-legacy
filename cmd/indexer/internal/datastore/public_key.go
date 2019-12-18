fbaf67c2dc53fa7e5367ed59bc1854fbc9cedf6f
Matúš Mrekaj <matus.mrekaj@icloud.com>
2019-12-18 21:41:49 +0100

    feat: add mocks for cmd/indexer (#446)
    * add mocks for clients package
    
    * add mocks for datastore package
    
    * add mocks for processor package
    
    * code fixes
    
    * gofmt

--- cmd/indexer/internal/datastore/public_key.go
+++ cmd/indexer/internal/datastore/public_key.go
@@ -6,6 +6,8 @@ import (
 	"github.com/mailchain/mailchain/crypto"
 )
 
+//go:generate mockgen -source=public_key.go -package=datastoretest -destination=./datastoretest/publick_key_mock.go
+
 type PublicKeyStore interface {
 	PutPublicKey(ctx context.Context, protocol, network string, address []byte, pubKey crypto.PublicKey) error
 	GetPublicKey(ctx context.Context, protocol, network string, address []byte) (pubKey crypto.PublicKey, err error)

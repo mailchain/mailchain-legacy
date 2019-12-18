d9be7169657d2aed4deb260cf8ee45d433b245e4
matusmrekaj <matus.mrekaj@icloud.com>
2019-12-18 20:55:14 +0100

    gofmt

--- cmd/indexer/internal/datastore/public_key.go
+++ cmd/indexer/internal/datastore/public_key.go
@@ -8,7 +8,6 @@ import (
 
 //go:generate mockgen -source=public_key.go -package=datastoretest -destination=./datastoretest/publick_key_mock.go
 
-
 type PublicKeyStore interface {
 	PutPublicKey(ctx context.Context, protocol, network string, address []byte, pubKey crypto.PublicKey) error
 	GetPublicKey(ctx context.Context, protocol, network string, address []byte) (pubKey crypto.PublicKey, err error)

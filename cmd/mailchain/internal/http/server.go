4b1c883cc6dfdbb173b5462e03edf4cd32231330
Pancakem <pancakesdeath@protonmail.com>
2019-12-28 22:21:55 +0300

    Add envelope handler to the API

--- cmd/mailchain/internal/http/server.go
+++ cmd/mailchain/internal/http/server.go
@@ -77,6 +77,7 @@ func CreateRouter(s *settings.Root, cmd *cobra.Command) (http.Handler, error) {
 	api.HandleFunc("/nameservice/address/{address}/resolve", handlers.GetResolveAddress(config.addressResolvers)).Methods("GET")
 	api.HandleFunc("/nameservice/name/{domain-name}/resolve", handlers.GetResolveName(config.domainResolvers)).Methods("GET")
 
+	api.HandleFunc("/envelope", handlers.GetEnvelope()).Methods("GET")
 	api.HandleFunc("/version", handlers.GetVersion()).Methods("GET")
 
 	_ = r.Walk(gorillaWalkFn)

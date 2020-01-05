6bdbabfe8c876a64680cb16db7a1f5bb0aa03296
Rob De Feo <robdefeo@gmail.com>
2020-01-05 14:46:40 +0000

    create key exchange

---
+++ crypto/cipher/keyexchange.go
@@ -0,0 +1,9 @@
+package cipher
+
+type KeyExchange interface {
+	// EphemeralKey generates a private/public key pair.
+	EphemeralKey() (private crypto.PrivateKey, public crypto.PublicKey, err error)
+
+	// SharedSecret computes a secret value from ephemeralKey private key and recipientKey public key.
+	SharedSecret(ephemeralKey crypto.PrivateKey, recipientKey crypto.PublicKey) ([]byte, error)
+}

8057a13a90706f7d5c08ec6fa1eaff3b9e8bc499
Developer Fred <developerfred@protonmail.ch>
2020-01-05 15:30:19 -0300

    feat: create a key exchange for ed25519 package #504 and sr25519 packange #505

---
+++ crypto/ed25519/keyexchange.go
@@ -0,0 +1,13 @@
+package ed25519
+
+import (
+	"github.com/mailchain/mailchain/crypto"
+)
+
+type KeyExchange interface {
+	// EphemeralKey generates a private/public key pair.
+	EphemeralKey() (private crypto.PrivateKey, public crypto.PublicKey, err error)
+
+	// SharedSecret computes a secret value from ephemeralKey private key and recipientKey public key.
+	SharedSecret(ephemeralKey crypto.PrivateKey, recipientKey crypto.PublicKey) ([]byte, error)
+}

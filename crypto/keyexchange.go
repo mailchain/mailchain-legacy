959ad8bc36c4dc43c881823d7f17f84f69fa0da2
Rob De Feo <robdefeo@gmail.com>
2020-01-05 16:42:26 +0000

    simpler interface for key exchange

--- crypto/keyexchange.go
+++ crypto/keyexchange.go
@@ -1,8 +1,9 @@
 package crypto
 
+// KeyExchange agrees on a symmetric keys by performing a key exchange using asymmetric keys.
 type KeyExchange interface {
 	// EphemeralKey generates a private/public key pair.
-	EphemeralKey() (private PrivateKey, public PublicKey, err error)
+	EphemeralKey() (private PrivateKey, err error)
 
 	// SharedSecret computes a secret value from ephemeralKey private key and recipientKey public key.
 	SharedSecret(ephemeralKey PrivateKey, recipientKey PublicKey) ([]byte, error)

b30a2c46594215de518629f48605f34d841cd934
@codingsh <developerfred@protonmail.ch>
2020-01-27 18:36:41 -0300

    Update cmd/mailchain/internal/settings/etherscan_pkf.go
    Co-Authored-By: Bot from GolangCI <42910462+golangcibot@users.noreply.github.com>

--- cmd/mailchain/internal/settings/etherscan_pkf.go
+++ cmd/mailchain/internal/settings/etherscan_pkf.go
@@ -4,6 +4,7 @@ import (
 	"github.com/mailchain/mailchain/cmd/mailchain/internal/settings/defaults"
 	"github.com/mailchain/mailchain/cmd/internal/settings/output"
 	"github.com/mailchain/mailchain/cmd/internal/settings/values"
+	"github.com/mailchain/mailchain/cmd/mailchain/internal/settings/defaults"
 	"github.com/mailchain/mailchain/internal/clients/etherscan"
 	"github.com/mailchain/mailchain/internal/mailbox"
 	"github.com/mailchain/mailchain/internal/protocols/ethereum"

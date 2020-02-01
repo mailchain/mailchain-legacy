453092f0e3047ab3298f26ef28af35dfa7c2dc7c
Nikos Kitmeridis <nikosk686@gmail.com>
2020-02-01 20:38:09 +0200

    #280 [WIP] Create a sender service for substrate
    - Removes subkey dependency
    - Coarse grained verify usage for testing singer sing method

--- internal/protocols/substrate/signer.go
+++ internal/protocols/substrate/signer.go
@@ -1,10 +1,8 @@
 package substrate
 
 import (
-	"github.com/centrifuge/go-substrate-rpc-client/signature"
 	"github.com/centrifuge/go-substrate-rpc-client/types"
 	"github.com/mailchain/mailchain/crypto"
-	"github.com/mailchain/mailchain/encoding"
 	"github.com/mailchain/mailchain/internal/mailbox/signer"
 	"github.com/pkg/errors"
 )
@@ -25,15 +23,45 @@ type Signer struct {
 func (e Signer) Sign(opts signer.Options) (signedTransaction interface{}, err error) {
 	switch opts := opts.(type) {
 	case SignerOptions:
-		pair, err := signature.KeyringPairFromSecret(encoding.EncodeHex(e.privateKey.Bytes()))
+		ext := &opts.Tx
+		o := opts.SignatureOptions
+		mb, err := types.EncodeToBytes(ext.Method)
 		if err != nil {
 			return nil, err
 		}
-		err = opts.Tx.Sign(pair, opts.SignatureOptions)
+		era := o.Era
+		if !o.Era.IsMortalEra {
+			era = types.ExtrinsicEra{IsImmortalEra: true}
+		}
+		payload := types.ExtrinsicPayloadV3{
+			Method:      mb,
+			Era:         era,
+			Nonce:       o.Nonce,
+			Tip:         o.Tip,
+			SpecVersion: o.SpecVersion,
+			GenesisHash: o.GenesisHash,
+			BlockHash:   o.BlockHash,
+		}
+		data, err := types.EncodeToBytes(payload)
 		if err != nil {
 			return nil, err
 		}
-		return &opts.Tx, nil
+		signedData, err := e.privateKey.Sign(data)
+		if err != nil {
+			return nil, err
+		}
+		sig := types.NewSignature(signedData)
+		signerPubKey := types.NewAddressFromAccountID(e.privateKey.PublicKey().Bytes())
+		extSig := types.ExtrinsicSignatureV4{
+			Signer:    signerPubKey,
+			Signature: types.MultiSignature{IsSr25519: true, AsSr25519: sig},
+			Era:       era,
+			Nonce:     o.Nonce,
+			Tip:       o.Tip,
+		}
+		ext.Signature = extSig
+		ext.Version |= types.ExtrinsicBitSigned
+		return ext, nil
 	default:
 		return nil, errors.New("invalid options for substrate signing")
 	}

fb73f742b8b97ec66410ac2a2b0cd8328b24795b
Matúš Mrekaj <matus.mrekaj@icloud.com>
2020-02-21 19:14:09 +0100

    add substrate to indexer

---
+++ cmd/indexer/internal/substrate/block.go
@@ -0,0 +1,43 @@
+package substrate
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/centrifuge/go-substrate-rpc-client/types"
+	"github.com/mailchain/mailchain/cmd/indexer/internal/actions"
+	"github.com/pkg/errors"
+)
+
+type Block struct {
+	txProcessor actions.Transaction
+}
+
+type TxOptions struct {
+	Block *types.Block
+}
+
+func NewBlockProcessor(tx actions.Transaction) *Block {
+	return &Block{
+		txProcessor: tx,
+	}
+}
+
+func (b *Block) Run(ctx context.Context, protocol, network string, blk interface{}) error {
+	sbrtBlk, ok := blk.(*types.Block)
+	if !ok {
+		return errors.New("tx must be go-substrate-rpc-client/types.Block")
+	}
+
+	fmt.Println("block hash: ", sbrtBlk.Header.Digest)
+
+	txs := sbrtBlk.Extrinsics
+	for i := range txs {
+		if err := b.txProcessor.Run(ctx, protocol, network, txs[i], &TxOptions{Block: sbrtBlk}); err != nil {
+			// TODO: fix error msg
+			return errors.Wrapf(err, "fails to process transaction hash: %s", nil)
+		}
+	}
+
+	return nil
+}

20b8a48a7401036f28066e85f1031bb16f6e39fe
Rob De Feo <robdefeo@gmail.com>
2020-07-30 21:43:28 +0100

    feat: retry database connections (#611)

--- cmd/internal/datastore/pq/connection.go
+++ cmd/internal/datastore/pq/connection.go
@@ -3,6 +3,7 @@ package pq
 import (
 	"fmt"
 
+	"github.com/cenkalti/backoff"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq" // needed for postgres to work
 	"github.com/pkg/errors"
@@ -13,9 +14,18 @@ func NewConnection(user, password, databaseName, host, sslmode string, port int)
 	// (in this case, `lib/pq`) used to register itself in `database/sql`.
 	// The next argument specifies the parameters to be used in the connection.
 	// Details about this string can be seen at https://godoc.org/github.com/lib/pq
-	db, err := sqlx.Connect("postgres", fmt.Sprintf(
-		"user=%s password=%s dbname=%s host=%s port=%d sslmode=%s",
-		user, password, databaseName, host, port, sslmode))
+	var db *sqlx.DB
+
+	operation := func() error {
+		var err error
+		db, err = sqlx.Connect("postgres", fmt.Sprintf(
+			"user=%s password=%s dbname=%s host=%s port=%d sslmode=%s",
+			user, password, databaseName, host, port, sslmode))
+
+		return err
+	}
+
+	err := backoff.Retry(operation, backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 5))
 
 	return db, errors.Wrapf(err, "could not open connection to postgres database: %s", databaseName)
 }

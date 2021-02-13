d4b88c61432f2a7cd482332ced91e7d11f6f959e
Rob De Feo <robdefeo@gmail.com>
2021-02-13 22:19:59 +0000

    fix: remove padding from base32 encoding (#671)

--- encoding/base32.go
+++ encoding/base32.go
@@ -7,7 +7,7 @@ import "encoding/base32"
 // DecodeBase32 expects that src contains only base32 characters.
 // If the input is malformed, DecodeBase32 returns an error.
 func DecodeBase32(src string) ([]byte, error) {
-	return base32.StdEncoding.DecodeString(src)
+	return base32.StdEncoding.WithPadding(base32.NoPadding).DecodeString(src)
 }
 
 // EncodeBase32 returns the string represented by the base32 byte src.
@@ -15,5 +15,5 @@ func DecodeBase32(src string) ([]byte, error) {
 // EncodeBase32 expects that src contains only base32 byte.
 // If the input is malformed, EncodeBase32 returns an error.
 func EncodeBase32(src []byte) string {
-	return base32.StdEncoding.EncodeToString(src)
+	return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(src)
 }

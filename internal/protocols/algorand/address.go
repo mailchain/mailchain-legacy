32a4ba6e983d50c7fe0e28925650893125e99b9b
Rob De Feo <robdefeo@gmail.com>
2021-02-14 00:01:23 +0000

    feat: address from public key function (#673)

---
+++ internal/protocols/algorand/address.go
@@ -0,0 +1,29 @@
+package algorand
+
+import (
+	"crypto/sha512"
+
+	"github.com/mailchain/mailchain/crypto"
+	"github.com/mailchain/mailchain/crypto/ed25519"
+	"github.com/pkg/errors"
+)
+
+const (
+	checksumLength = 4
+)
+
+// Address returns the address from the public key.
+func Address(pubKey crypto.PublicKey) ([]byte, error) {
+	switch pubKey.(type) {
+	case ed25519.PublicKey, *ed25519.PublicKey:
+		return append(pubKey.Bytes(), checksum(pubKey.Bytes())...), nil
+	default:
+		return nil, errors.Errorf("invalid public key type: %T", pubKey)
+	}
+}
+
+// Algorand 4-byte checksum added to public key to make an algorand address.
+func checksum(data []byte) []byte {
+	fullHash := sha512.Sum512_256(data)
+	return fullHash[len(fullHash)-checksumLength:]
+}

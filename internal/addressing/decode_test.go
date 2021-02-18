472fb35494c3e84c9ffb02f1c235ec54d259d93b
Rob De Feo <robdefeo@gmail.com>
2021-02-18 23:29:40 +0000

    fix tests

--- internal/addressing/decode_test.go
+++ internal/addressing/decode_test.go
@@ -15,10 +15,10 @@
 package addressing
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/mailchain/mailchain/encoding/encodingtest"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDecodeByProtocol(t *testing.T) {
@@ -47,7 +47,7 @@ func TestDecodeByProtocol(t *testing.T) {
 				"5DJJhV3tVzsWG1jZfL157azn8iRyDC7HyNG1yh8v2nQYd994",
 				"substrate",
 			},
-			encodingtest.MustDecodeBase32("5DJJhV3tVzsWG1jZfL157azn8iRyDC7HyNG1yh8v2nQYd994"),
+			encodingtest.MustDecodeBase58("5DJJhV3tVzsWG1jZfL157azn8iRyDC7HyNG1yh8v2nQYd994"),
 			false,
 		},
 		{
@@ -56,7 +56,7 @@ func TestDecodeByProtocol(t *testing.T) {
 				"C7Z4NNMIMOGZW56JCILF6DVY4MBZJMHXUQ67W2WKVE6U5QJSIDPYUEAXQU",
 				"algorand",
 			},
-			encodingtest.MustDecodeBase32("C7Z4NNMIMOGZW56JCILF6DVY4MBZJMHXUQ67W2WKVE6U5QJSIDPYUEAXQU"),
+			[]byte{0x17, 0xf3, 0xc6, 0xb5, 0x88, 0x63, 0x8d, 0x9b, 0x77, 0xc9, 0x12, 0x16, 0x5f, 0xe, 0xb8, 0xe3, 0x3, 0x94, 0xb0, 0xf7, 0xa4, 0x3d, 0xfb, 0x6a, 0xca, 0xa9, 0x3d, 0x4e, 0xc1, 0x32, 0x40, 0xdf, 0x8a, 0x10, 0x17, 0x85},
 			false,
 		},
 		{
@@ -76,7 +76,7 @@ func TestDecodeByProtocol(t *testing.T) {
 				t.Errorf("DecodeByProtocol() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
+			if !assert.Equal(t, tt.want, got) {
 				t.Errorf("DecodeByProtocol() = %v, want %v", got, tt.want)
 			}
 		})

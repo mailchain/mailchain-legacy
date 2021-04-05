b776e10d03e066e166a5008551f8cd57e5d5139a
Shivani Aggarwal <shivani.aggarwal2422@gmail.com>
2021-04-04 23:12:03 -0400

    Balance Finders Test

---
+++ cmd/mailchain/internal/settings/bf.go
@@ -0,0 +1,49 @@
+package settings
+
+import (
+	"github.com/mailchain/mailchain/cmd/internal/settings/output"
+	"github.com/mailchain/mailchain/cmd/internal/settings/values"
+	"github.com/mailchain/mailchain/cmd/mailchain/internal/settings/defaults"
+	"github.com/mailchain/mailchain/internal/mailbox"
+	"github.com/pkg/errors"
+)
+
+func balanceFinders(s values.Store) *BalanceFinders {
+	return &BalanceFinders{
+		clients: map[string]BalanceFinderClient{
+			defaults.ClientEtherscanNoAuth: etherscanBalanceFinderNoAuth(s),
+			defaults.ClientEtherscan:       etherscanBalanceFinder(s),
+			// defaults.ClientBlockscoutNoAuth:   etherscanBalanceFinderAny(s),
+			// defaults.SubstratePublicKeyFinder: EtherscanBalanceFinder(s),
+		},
+	}
+}
+
+// BalanceFinders configuration element.
+type BalanceFinders struct {
+	clients map[string]BalanceFinderClient
+}
+
+// Produce `mailbox.PublicKeyFinder` based on configuration settings.
+func (s BalanceFinders) Produce(client string) (mailbox.Balance, error) {
+	if client == "" {
+		return nil, nil
+	}
+	m, ok := s.clients[client]
+	if !ok {
+		return nil, errors.Errorf("%s not a supported balance finder", client)
+	}
+	return m.Produce()
+}
+
+// Output configuration as an `output.Element` for use in exporting configuration.
+func (s BalanceFinders) Output() output.Element {
+	elements := []output.Element{}
+	for _, c := range s.clients {
+		elements = append(elements, c.Output())
+	}
+	return output.Element{
+		FullName: "balance-finders",
+		Elements: elements,
+	}
+}

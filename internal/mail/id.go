57b52dfc5dd466de81b7eeb4bf4dccab762a6831
Rob De Feo <robdefeo@gmail.com>
2022-01-31 11:40:06 +0000

    chore: update license

--- internal/mail/id.go
+++ internal/mail/id.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Finobo
+// Copyright 2022 Mailchain Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@ package mail
 
 import (
 	"crypto/rand"
+
 	"github.com/mailchain/mailchain/encoding"
 	"github.com/pkg/errors"
 )
